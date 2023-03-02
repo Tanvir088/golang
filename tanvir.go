70a1854c704ea21a6257cd1295ef0e8a1b730af3
“Tanvir_Ahammed” <tsourov809@gmail.com>
2023-03-03 05:35:58 +0600

    test

---
+++ tanvir.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	var name, city, district, country string = "Tanvir", "Rangpur", "Dhaka", "Bangladesh"
+	fmt.Println(name, city, district, country)
+
+}
