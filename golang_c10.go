f4bc47ccfab7dbe90281e706fede8731be38d959
“Tanvir_Ahammed” <tsourov809@gmail.com>
2023-03-03 19:48:05 +0600

    golang_c10.go

---
+++ golang_c10.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var students [5]string
+	students[0] = "jamil"
+	students[1] = "kamal"
+	students[2] = "rafi"
+	students[3] = "safi"
+	students[4] = "manik"
+	fmt.Println(students[4])
+}
