30d907b9374b083d686136e9bd4126806a46f887
“Tanvir_Ahammed” <tsourov809@gmail.com>
2023-03-04 19:29:29 +0600

    slice shortcut

--- array_slice.go
+++ array_slice.go
@@ -1,10 +1,7 @@
 package main 
 import "fmt"
 func main(){
-var students [3]string
-  students[0]="tanvir"
-   students[1]="ahammed"
-   students[2]="sourov"
- x:= students[0:2]
-fmt.Println(x)
+var fruits[]string
+fruits = append(fruits,"Apple","mango","nut")
+fmt.Println(fruits)
 }
